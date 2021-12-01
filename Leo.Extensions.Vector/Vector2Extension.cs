f89056487315a5fdccf23e140135ba13b9d7c8e7
Leo.Wan <1830802211@qq.com>
2021-12-01 13:40:12 +0800

    feat(all): init

---
+++ Leo.Extensions.Vector/Vector2Extension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leo.Extensions
+{
+    public static class Vector2Extension
+    {
+        public static Vector2 Vector2Lerp(this float amount, Vector2 v1, Vector2 v2)
+        {
+            return Vector2.Lerp(v1, v2, amount);
+        }
+    }
+}
---
+++ Leo.Extensions.Vector/Vector3Extension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leo.Extensions
+{
+    public static class Vector3Extension
+    {
+        public static Vector3 Vector2Lerp(this float amount, Vector3 v1, Vector3 v2)
+        {
+            return Vector3.Lerp(v1, v2, amount);
+        }
+    }
+}
